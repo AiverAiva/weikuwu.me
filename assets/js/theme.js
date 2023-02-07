de73eeb7b69ed0d87efc69798fbd7ee7357d64bb
WeiKuOuO <43096905+AiverAiva@users.noreply.github.com>
2023-02-07 08:30:38 +0800

    7

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -6,35 +6,12 @@ $('body').on('click', function (e) {
             $(this).popover('hide');
         }
     });
+    $("[data-toggle='tooltip']").each(function () {
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.tooltip').has(e.target).length === 0) {
+            $(this).tooltip('disable');
+        }
+    });
 });
-
-var content= document.createElement('div');
-content.innerHTML = "AiverAiva#1234 <a tabindex='0' id='copy' data-clipboard-text='AiverAiva#1234'><i class='bx bxs-copy' id='copy-button'></i></a>"
-// AiverAiva#1234 
-// onclick='copyDiscordTag()' data-toggle='tooltip' data-placement='top' title='Tooltip on top'
-$('#shapes-trigger').tooltip({
-    title: 'Shapes'
-}).popover({
-    html : true,
-    // title: boomHTML,
-    content: content,
-    placement: 'top'
-}).on('show.bs.popover', function() {
-    $(this).tooltip('hide');
-    $('[data-toggle="tooltip"]').tooltip('disable');
-}).on('hide.bs.popover', function() {
-    $('[data-toggle="tooltip"]').tooltip('enable');
-});
-
-// (function(){
-// 	new Clipboard('#copy-button');
-// })();
-
-function copyDiscordTag() {
-    navigator.clipboard.writeText('AiverAiva#1234')
-    // var tooltip = document.getElementById("myTooltip");
-    // tooltip.innerHTML = "Copied: " + copyText;
-}
 
 $('#copy').tooltip({
     trigger: 'click',
