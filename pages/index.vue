6b59bd0198bec6b19eb96da232857f67831fec4d
WeiKuOuO <43096905+AiverAiva@users.noreply.github.com>
2023-05-21 07:57:59 +0800

    qwq

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,58 @@
+<!-- //
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//
+//
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//               佛祖保佑         永無BUG
+//
+// -->
+
 <template>
   <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
   <div id="header" class="flex flex-col justify-center">
     <nav id="navbar" class="navbar nav-menu">
       <ul>
         <li><a href="#hero" :class="{active: activeSection === 'hero'}"><i class="bx bx-home"></i> <span>Home</span></a></li>
+        <li><a href="#skillsets" :class="{active: activeSection === 'skillsets'}"><i class='bx bx-id-card'></i><span>Skillsets</span></a></li>
         <!-- <li><a href="#games" :class="{active: activeSection === 'games'}"><i class='bx bx-joystick'></i><span>Games</span></a></li> -->
       </ul>
     </nav>
   </div>
 
-  
   <hero id="hero"/>
+  <skillsets id="skillsets"/>
   <!-- <games id="games"/> -->
 </template>
 
 <script>
   import games from './games.vue'
   import hero from './hero.vue'
+  import skillsets from './skillsets.vue'
 
   export default {
     name: 'index',
     components: { 
       games,
       hero,
+      skillsets
     },
     data() {
       return {
@@ -38,12 +68,12 @@
     methods: {
       handleScroll() {
         const hero = document.querySelector('#hero')
-        const games = document.querySelector('#games')
+        const skillsets = document.querySelector('#skillsets')
 
         if (this.isScrolledIntoView(hero)) {
           this.activeSection = 'hero'
-        } else if (this.isScrolledIntoView(games)) {
-          this.activeSection = 'games'
+        } else if (this.isScrolledIntoView(skillsets)) {
+          this.activeSection = 'skillsets'
         }
       },
       isScrolledIntoView(el) {
@@ -57,3 +87,4 @@
     },
   }
 </script>
+
