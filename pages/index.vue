fc466af663718937cb1faa4aba808dc7ce339c14
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2023-09-30 07:40:16 +0800

    Update index.vue
    fix: layout duplication from ssr not syncing in server side rendering and client side rendering, ssr issue

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,18 @@
 <template>
-  <body>
-    <!-- <i class="bx bx-list-ul mobile-nav-toggle d-xl-none"></i> -->
-    <MobileNavToggle />
-    <header id="header" class="flex flex-col justify-center">
-      <nav id="navbar" class="navbar nav-menu">
-        <ul>
-          <li><a href="#hero" :class="{active: activeSection === 'hero'}"><i class="bx bx-home"></i> <span>Home</span></a></li>
-          <li><a href="#skillsets" :class="{active: activeSection === 'skillsets'}"><i class='bx bx-id-card'></i><span>Skillsets</span></a></li>
-          <!-- <li><a href="#games" :class="{active: activeSection === 'games'}"><i class='bx bx-joystick'></i><span>Games</span></a></li> -->
-        </ul>
-      </nav>
-    </header>
+  <MobileNavToggle />
+  <header id="header" class="flex flex-col justify-center">
+    <nav id="navbar" class="navbar nav-menu">
+      <ul>
+        <li><a href="#hero" :class="{active: activeSection === 'hero'}"><i class="bx bx-home"></i> <span>Home</span></a></li>
+        <!-- <li><a href="#skillsets" :class="{active: activeSection === 'skillsets'}"><i class='bx bx-id-card'></i><span>Skillsets</span></a></li> -->
+        <!-- <li><a href="#games" :class="{active: activeSection === 'games'}"><i class='bx bx-joystick'></i><span>Games</span></a></li> -->
+      </ul>
+    </nav>
+  </header>
 
-    <hero id="hero"/>
-    <skillsets id="skillsets"/>
+  <hero id="hero"/>
+  <!-- <skillsets id="skillsets"/> -->
   <!-- <games id="games"/> -->
-  </body>
-  
 </template>
 
 <script>
