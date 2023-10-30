23a94123f2cf831a18146509d5c150397f985919
WeiKuOuO <43096905+AiverAiva@users.noreply.github.com>
2023-10-30 19:25:52 +0800

    feat: theme switcher

---
+++ components/ThemeSwitcher.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="fixed top-4 left-4 z-50">
+        <button @click="toggleTheme" aria-label="Toggle dark mode" class="theme-switcher-button"
+            :class="{ 'dark-mode-button': currentTheme === 'dark', 'light-mode-button': currentTheme === 'light' }">
+            <i class="bx" :class="{ 'bx-sun': currentTheme === 'light', 'bx-moon': currentTheme === 'dark' }"></i>
+        </button>
+    </div>
+</template>
+  
+<script setup>
+import { ref } from 'vue';
+
+const currentTheme = ref('light');
+
+function toggleTheme() {
+    currentTheme.value = currentTheme.value === 'light' ? 'dark' : 'light';
+}
+
+watch(currentTheme, (newValue) => {
+    console.log(newValue)
+    if (newValue == "dark") {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+});
+</script>
+  
