7312d964f907e38a3c0d3861547f3816c314d3fb
WeiKuOuO <43096905+AiverAiva@users.noreply.github.com>
2023-11-08 03:25:39 +0800

    fix: navbar active color not changing on specific height and make it scroll instead of teleporting to the part

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,47 +1,53 @@
+
 <template>
     <nav id="navbar" class="navbar nav-menu">
         <ul>
-            <li><a href="#hero" :class="{ active: activeSection === 'hero' }"><i class="bx bx-home"></i> <span>Home</span></a></li>
-            <li><a href="#about" :class="{ active: activeSection === 'about' }"><i class='bx bx-id-card'></i><span>About</span></a></li>
-            <!-- <li><a href="#games" :class="{active: activeSection === 'games'}"><i class='bx bx-joystick'></i><span>Games</span></a></li> -->
+            <li><a :class="{ active: activeSection === 'hero' }" @click="scrollToSection('hero')"><i class="bx bx-home"></i>
+                    <span>Home</span></a></li>
+            <li><a :class="{ active: activeSection === 'about' }" @click="scrollToSection('about')"><i
+                        class='bx bx-id-card'></i><span>About</span></a></li>
         </ul>
     </nav>
 </template>
-
-
+  
 <script>
-
 export default {
     data() {
         return {
-            activeSection: "hero",
-        }
+            activeSection: 'hero',
+            sectionIds: ['hero', 'about'],
+        };
     },
     mounted() {
-        window.addEventListener('scroll', this.handleScroll)
+        window.addEventListener('scroll', this.handleScroll);
     },
     beforeDestroy() {
-        window.removeEventListener('scroll', this.handleScroll)
+        window.removeEventListener('scroll', this.handleScroll);
     },
     methods: {
         handleScroll() {
-            const hero = document.querySelector('#hero')
-            const skillsets = document.querySelector('#about')
-
-            if (this.isScrolledIntoView(hero)) {
-                this.activeSection = 'hero'
-            } else if (this.isScrolledIntoView(skillsets)) {
-                this.activeSection = 'about'
+            for (const sectionId of this.sectionIds) {
+                const section = document.querySelector(`#${sectionId}`);
+                if (this.isScrolledIntoView(section)) {
+                    this.activeSection = sectionId;
+                    break;
+                }
             }
         },
         isScrolledIntoView(el) {
-            const rect = el.getBoundingClientRect()
-            const elemTop = rect.top
-            const elemBottom = rect.bottom
-
-            const isVisible = elemTop >= 0 && elemBottom <= window.innerHeight
-            return isVisible
+            if (!el) return false;
+            const rect = el.getBoundingClientRect();
+            return rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2;
+        },
+        scrollToSection(section) {
+            const targetElement = document.querySelector(`#${section}`);
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth',
+                });
+                this.activeSection = section;
+            }
         },
     },
-}
+};
 </script>
