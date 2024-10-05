f5ca292aa5c9f5da1ae0044bdae5c9f193280754
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-10-05 19:19:14 +0800

    feat: layout for posts, WIP

---
+++ layouts/post.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <NuxtPage />
+    </div>
+</template>
+
+<script setup>
+// useSeoMeta({
+//     url: 'https://weikuwu.me',
+//     title: 'AiverAiva',
+//     ogTitle: 'AiverAiva',
+//     description: 'I\'m a student from Taiwan :3',
+//     ogDescription: 'I\'m a student from Taiwan :3',
+//     theme_color: "#ffb3c6",      
+//     ogImage: '/img/thumbnail.png',
+//     // twitterCard: 'summary_large_image',
+// })
+</script>
