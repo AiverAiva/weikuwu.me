52ae2e38e95eade62b2a22d1016835c027566abc
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-10-05 19:17:37 +0800

    feat: Back button

---
+++ components/Button/Back.vue
@@ -0,0 +1,8 @@
+<template>
+    <button @click.self="$router.back()" class="text-slate-800 font-mono py-1 px-3 rounded hover:bg-pink-100 transition mb-2">
+        &larr; Back
+    </button>
+</template>
+
+<script>
+</script>
