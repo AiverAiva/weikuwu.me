142e380bc5dfdb30f791f54a69af7e8b5c1e54d2
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-10-18 09:35:59 +0800

    refactor: make post page work and get the back button working properly on special cases

--- components/Button/Back.vue
+++ components/Button/Back.vue
@@ -1,9 +1,17 @@
 <template>
-    <button @click.self="$router.back()"
-        class="text-slate-800 font-mono py-1 px-3 rounded hover:bg-opacity-65 hover:bg-red-200 transition m-2 dark:text-slate-100">
-        &larr; Back
-    </button>
+    <a :href="href">
+        <button
+            class="text-slate-800 font-mono py-1 px-3 rounded hover:bg-opacity-65 hover:bg-red-200 transition m-2 dark:text-slate-100">
+            &larr; Back
+        </button>
+    </a>
 </template>
 
-<script>
+<script setup>
+const props = defineProps({
+    href: {
+        type: String,
+        default: ""
+    }
+});
 </script>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,9 +1,44 @@
 <template>
-    <PostContainer v-for="p in posts" v-bind:key="p.slug" :post="p.title" :slug="p._path.split('/').pop()" />
+    <div class="
+        w-screen
+        h-screen
+        fixed
+        top-0
+        left-0
+        z-10
+        bg-gray-200 
+        bg-opacity-50
+        dark:bg-slate-900
+        flex
+        justify-center
+        items-center
+        duration-150
+      ">
+        <ThemeSwitcher />
+        <ButtonBack href="/"/>
+        <div class="
+          w-full
+          max-w-screen-lg
+          overflow-y-auto
+          bg-white
+          dark:bg-slate-800
+          p-6
+          mx-4
+          rounded-xl
+          shadow-lg
+          duration-150
+        " style="max-height: calc(80vh)">
+            <PostContainer v-for="p in posts" v-bind:key="p.slug" :post="p" :slug="p._path.split('/').pop()" />
+        </div>
+    </div>
 </template>
  
 <script setup>
-const { data: posts } = await useAsyncData('posts', () => queryContent('/posts').find());
+const { data: posts } = await useAsyncData('post', () => queryContent('/post').find());
+
+onMounted(() => {
+    document.body.classList.add('dark');
+})
 </script>
 
 <style scoped>
